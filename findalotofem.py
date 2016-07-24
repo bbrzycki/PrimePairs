4de9ddbdb8a8475c0f30793eeb7fbf5cb193cf8f
Bryan Brzycki <bryan6brzycki@gmail.com>
2016-07-24 02:46:15 -0400

    Add index_limit

--- findalotofem.py
+++ findalotofem.py
@@ -3,19 +3,18 @@ import numpy as np
 import isprime as isp
 import findbroprime as f
 
-def runOnPrimes(start,stop):
+def runOnPrimes(start,stop,index_limit):
 	count = 0
 	for i in xrange(start,stop+1):
 		if isp.isPrime(i):
-			bro_candidate = f.findBroPrime(i)
+			bro_candidate = f.findBroPrime(i,index_limit)
 			#print i, bro_candidate
 			if bro_candidate == -1:
 				count += 1
 	return count
 
-print runOnPrimes(2,1000000)
 
-def runOnPrimesSet(start,stop):
+def runOnPrimesSet(start,stop,index_limit):
 	broprimes = set()
 	for i in xrange(start,stop+1):
 		if isp.isPrime(i):
@@ -27,7 +26,7 @@ def runOnPrimesSet(start,stop):
 				elif i == b:
 					print b, a
 					broprimeFound = True
-			bro_candidate = f.findBroPrime(i)
+			bro_candidate = f.findBroPrime(i,index_limit)
 			if broprimeFound == False:
 				
 				if bro_candidate == -1:
@@ -37,7 +36,7 @@ def runOnPrimesSet(start,stop):
 			if bro_candidate == -1:
 					print "Not found for ", i, "!!"
 			elif bro_candidate != -1:
-				broprimes.add((i,f.findBroPrime(i)))
+				broprimes.add((i,f.findBroPrime(i,index_limit)))
 				print i, bro_candidate
 	return
 
