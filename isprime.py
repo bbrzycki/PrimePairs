bd4e6310464c96e7eb567d24b3bd8764f6f784f8
Alan Bid <alanbidart@gmail.com>
2016-07-23 23:53:41 -0400

    improved primality test

--- isprime.py
+++ isprime.py
@@ -2,7 +2,12 @@ import math as m
 import numpy as np
 
 def isPrime(number):
-	for i in xrange(2,int(m.sqrt(number))+1):
-		if number % i == 0:
-			return False
+	if number <= 1 return False
+	elif number <= 3 return True
+	elif number % 2 == 0 or number % 3 == 0 return False
+    	i = 5
+    	while i×i <= number
+        	if number % i == 0 or number % (i + 2) == 0
+            		return False
+        	i = i + 6
 	return True
