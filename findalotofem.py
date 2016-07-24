c0ff7b191d636b14738a79879e2618e262fcc3fd
Bryan Brzycki <bryan6brzycki@gmail.com>
2016-07-24 01:39:02 -0400

    Account for silly overflow error

--- findalotofem.py
+++ findalotofem.py
@@ -3,3 +3,9 @@ import numpy as np
 import isprime as isp
 import findbroprime as f
 
+def runOnPrimes(start,stop):
+	for i in xrange(start,stop+1):
+		if isp.isPrime(i):
+			print i, f.findBroPrime(i)
+	return 
+
--- findbroprime.py
+++ findbroprime.py
@@ -1,6 +1,9 @@
 import math as m
 import numpy as np
 import isprime as isp
+import warnings
+
+# warnings.filterwarnings('ignore')
 
 def findBroPrime(prime):
 	foundPrime = False
@@ -9,8 +12,12 @@ def findBroPrime(prime):
 	while foundPrime == False:
 		if isp.isPrime(temp_prime):
 			foundPrime = True
-			print index, temp_prime
+			# print index, temp_prime
 			return temp_prime
+		elif index >= 62:
+			return "None found with index at most 62"
 		else:
 			index += 1
-			temp_prime = prime + np.power(2, index)
+			temp_prime = prime + np.power(2,index)
+		if index % 1 ==0: print index, temp_prime
+
