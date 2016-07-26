484f3b95faa08aed92968a4db86d78c584112fc0
Bryan Brzycki <bryan6brzycki@gmail.com>
2016-07-26 03:29:06 -0400

    Update isprime

--- isprime.py
+++ isprime.py
@@ -11,3 +11,22 @@ def isPrime(number):
 			return False
         	i = i + 6
 	return True
+
+def isPrimeArray(number, prime_list):
+	if number in prime_list:
+		return True
+	elif number < prime_list[-1]:
+		return False
+	else:
+		for prime in prime_list:
+			if number % prime == 0:
+				return False
+		if number <= 1: return False
+		elif number <= 3: return True
+		elif number % 2 == 0 or number % 3 == 0: return False
+    		i = max(5,prime_list[-1])
+    		while i**2 <= number:
+        		if number % i == 0 or number % (i + 2) == 0:
+				return False
+        		i = i + 6
+		return True
